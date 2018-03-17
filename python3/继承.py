b686e76130fc92a3be8269ac7bb66438b75d578d
Leochens <18332518328@163.com>
2018-03-17 21:02:45 +0800

    继承完善

--- python3/继承.py
+++ python3/继承.py
@@ -8,13 +8,25 @@ class Friut:
 
     def sayname(self):
         print(self.name)
+    def hello(self):
+        print('hello i am {}'.format(self.name))
 
 # 子类 括号里写父类名字
 class Apple(Friut):
+
+    # 重写父类属性
     name = 'apple'
 
+    # 重写父类方法
+    def hello(self):
+        super().hello()         #调用父类的方法
+        print('there is child')
 
 apple = Apple()
 
 # 调用父类方法
 apple.sayname()
+
+# 调用重写后的父类方法
+
+apple.hello()
